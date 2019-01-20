d557c86d78d5392cc9938755e42e595a1d3c7f51
Jacob <jacob.goodridge@hotmail.com>
2019-01-20 15:26:57 +1100

    Fixing indents~

--- Bot.py
+++ Bot.py
@@ -10,13 +10,14 @@ from time import sleep # sleep
 t = auth.login() #In order to log into Twitter.
 ok = True #Just for loop start
 
-		
+
 tweet = input('What would you like to tweet? ')
-	if ok == True: #making sure ok is true	
-		
-	t.update_status(tweet) #Posts the tweet, using t.	
-	print(f'Completed! Posted "{tweet}")
-	      
-	      
-	except:
-		pass #If fails, just ignore and keep going.
+if ok == True: #making sure ok is true	
+
+t.update_status(tweet) #Posts the tweet, using t.	
+print(f'Completed! Posted "{tweet}")
+
+
+  
+except:
+	pass #If fails, just ignore and keep going.
